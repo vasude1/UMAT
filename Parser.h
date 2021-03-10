df24358656be792f3203c08eefbbf53a7b542d03
Vasudevan <31038144+vasude1@users.noreply.github.com>
2021-03-10 22:13:39 +0100

    Add files via upload

--- Parser.h
+++ Parser.h
@@ -33,30 +33,30 @@ void parse_material_properties(double* props, double* stiff_ratio, double* relax
 
 void parse_internal_variables(double* STATEV,Matrix3d* ivar, int number_branches){
   for(int i=0;i<number_branches;++i){
-    *(ivar+i) << *(STATEV+9*i),*(STATEV+9*i+1),*(STATEV+9*i+2),
-                *(STATEV+9*i+3),*(STATEV+9*i+4),*(STATEV+9*i+5),
-                *(STATEV+9*i+6),*(STATEV+9*i+7),*(STATEV+9*i+8);
+    *(ivar+i) << *(STATEV+10*i),*(STATEV+10*i+1),*(STATEV+10*i+2),
+                *(STATEV+10*i+3),*(STATEV+10*i+4),*(STATEV+10*i+5),
+                *(STATEV+10*i+6),*(STATEV+10*i+7),*(STATEV+10*i+8);
    *(ivar+i) += MatrixXd::Identity(3,3);
    // cout<<*(ivar+i)<<endl;
   }
 };
 
-void deviatoric_projector(MatrixXd& tau){
-  tau -= 1.0/3.0*(tau(0,0)+tau(1,1)+tau(2,2))*MatrixXd::Identity(3,3);
+void deviatoric_projector(Matrix3d* tau){
+  (*tau) -= 1.0/3.0*((*tau)(0,0)+(*tau)(1,1)+(*tau)(2,2))*MatrixXd::Identity(3,3);
 };
 
 
-void deviatoric_projector_tangent(double p,MatrixXd tau,MatrixXd& tangent, MatrixXd& C_inv){
-  MatrixXd tangent_temp = tangent;
-  tangent.col(0).array() -= (tangent_temp.col(0).array() + tangent_temp.col(1).array() + tangent_temp.col(2).array())/3.0;
-  tangent.col(2).array() -= (tangent_temp.col(0).array() + tangent_temp.col(1).array() + tangent_temp.col(2).array())/3.0;
-  tangent.col(3).array() -= (tangent_temp.col(0).array() + tangent_temp.col(1).array() + tangent_temp.col(2).array())/3.0;
+void deviatoric_projector_tangent(double p,Matrix3d tau,Matrix6d* tangent){
+  MatrixXd tangent_temp = *tangent;
+  (*tangent).col(0).array() -= (tangent_temp.col(0).array() + tangent_temp.col(1).array() + tangent_temp.col(2).array())/3.0;
+  (*tangent).col(2).array() -= (tangent_temp.col(0).array() + tangent_temp.col(1).array() + tangent_temp.col(2).array())/3.0;
+  (*tangent).col(3).array() -= (tangent_temp.col(0).array() + tangent_temp.col(1).array() + tangent_temp.col(2).array())/3.0;
 
-  tangent.row(0).array() -= (tangent_temp.row(0).array() + tangent_temp.row(1).array() + tangent_temp.row(2).array())/3.0;
-  tangent.row(2).array() -= (tangent_temp.row(0).array() + tangent_temp.row(1).array() + tangent_temp.row(2).array())/3.0;
-  tangent.row(3).array() -= (tangent_temp.row(0).array() + tangent_temp.row(1).array() + tangent_temp.row(2).array())/3.0;
+  (*tangent).row(0).array() -= (tangent_temp.row(0).array() + tangent_temp.row(1).array() + tangent_temp.row(2).array())/3.0;
+  (*tangent).row(2).array() -= (tangent_temp.row(0).array() + tangent_temp.row(1).array() + tangent_temp.row(2).array())/3.0;
+  (*tangent).row(3).array() -= (tangent_temp.row(0).array() + tangent_temp.row(1).array() + tangent_temp.row(2).array())/3.0;
 
-  tangent(seq(0,2),seq(0,2)).array() += tangent_temp(seq(0,2),seq(0,2)).sum()/9.0;
+  (*tangent)(seq(0,2),seq(0,2)).array() += tangent_temp(seq(0,2),seq(0,2)).sum()/9.0;
 
   VectorXd tau_iso(6);
   double tr_tau = tau(0,0)+tau(1,1)+tau(2,2);
@@ -70,44 +70,44 @@ void deviatoric_projector_tangent(double p,MatrixXd tau,MatrixXd& tangent, Matri
 
   // cout<<tau_iso<<endl;
 
-  tangent(seq(0,5),0) -= 2.0/3.0*tau_iso;
-  tangent(seq(0,5),1) -= 2.0/3.0*tau_iso;
-  tangent(seq(0,5),2) -= 2.0/3.0*tau_iso;
+  (*tangent)(seq(0,5),0) -= 2.0/3.0*tau_iso;
+  (*tangent)(seq(0,5),1) -= 2.0/3.0*tau_iso;
+  (*tangent)(seq(0,5),2) -= 2.0/3.0*tau_iso;
 
-  tangent(0,seq(0,5)) -= 2.0/3.0*tau_iso;
-  tangent(1,seq(0,5)) -= 2.0/3.0*tau_iso;
-  tangent(2,seq(0,5)) -= 2.0/3.0*tau_iso;
+  (*tangent)(0,seq(0,5)) -= 2.0/3.0*tau_iso;
+  (*tangent)(1,seq(0,5)) -= 2.0/3.0*tau_iso;
+  (*tangent)(2,seq(0,5)) -= 2.0/3.0*tau_iso;
   //
 
   tr_tau *= 2.0/3.0;
   //
-  tangent(0,0) += 2.0/3.0*tr_tau;
-  tangent(0,1) += -1.0/3.0*tr_tau;
-  tangent(0,2) += -1.0/3.0*tr_tau;
+  (*tangent)(0,0) += 2.0/3.0*tr_tau;
+  (*tangent)(0,1) += -1.0/3.0*tr_tau;
+  (*tangent)(0,2) += -1.0/3.0*tr_tau;
 
-  tangent(1,0) += -1.0/3.0*tr_tau;
-  tangent(1,1) += 2.0/3.0*tr_tau;
-  tangent(1,2) += -1.0/3.0*tr_tau;
+  (*tangent)(1,0) += -1.0/3.0*tr_tau;
+  (*tangent)(1,1) += 2.0/3.0*tr_tau;
+  (*tangent)(1,2) += -1.0/3.0*tr_tau;
 
-  tangent(2,0) += -1.0/3.0*tr_tau;
-  tangent(2,1) += -1.0/3.0*tr_tau;
-  tangent(2,2) += 2.0/3.0*tr_tau;
+  (*tangent)(2,0) += -1.0/3.0*tr_tau;
+  (*tangent)(2,1) += -1.0/3.0*tr_tau;
+  (*tangent)(2,2) += 2.0/3.0*tr_tau;
 
-  tangent(3,3) += 0.5*tr_tau;
-  tangent(4,4) += 0.5*tr_tau;
-  tangent(5,5) += 0.5*tr_tau;
+  (*tangent)(3,3) += 0.5*tr_tau;
+  (*tangent)(4,4) += 0.5*tr_tau;
+  (*tangent)(5,5) += 0.5*tr_tau;
 
 
   //
   for(int i =0; i<3;++i){
-      tangent(i,i) += -2.0*p;
+      (*tangent)(i,i) += -2.0*p;
   }
 
-  tangent(3,3) += -p;
-  tangent(4,4) += -p;
-  tangent(5,5) += -p;
+  (*tangent)(3,3) += -p;
+  (*tangent)(4,4) += -p;
+  (*tangent)(5,5) += -p;
 
-  tangent(seq(0,2),seq(0,2)).array() += p;
+  (*tangent)(seq(0,2),seq(0,2)).array() += p;
   // //
   VectorXd tau_row(6);
   // tau_row = tau_iso;
@@ -122,32 +122,32 @@ void deviatoric_projector_tangent(double p,MatrixXd tau,MatrixXd& tangent, Matri
   // //
 
 
-  tangent(0,0) += 2*tau_row(0);
-  tangent(1,1) += 2*tau_row(1);
-  tangent(2,2) += 2*tau_row(2);
-  tangent(3,3) += 0.5*(tau_row(0)+tau_row(1));
-  tangent(4,4) += 0.5*(tau_row(1)+tau_row(2));
-  tangent(5,5) += 0.5*(tau_row(0)+tau_row(2));
+  (*tangent)(0,0) += 2*tau_row(0);
+  (*tangent)(1,1) += 2*tau_row(1);
+  (*tangent)(2,2) += 2*tau_row(2);
+  (*tangent)(3,3) += 0.5*(tau_row(0)+tau_row(1));
+  (*tangent)(4,4) += 0.5*(tau_row(1)+tau_row(2));
+  (*tangent)(5,5) += 0.5*(tau_row(0)+tau_row(2));
 
-  tangent(3,0) += tau_row(3);
-  tangent(5,0) += tau_row(5);
-  tangent(3,1) += tau_row(3);
-  tangent(4,1) += tau_row(4);
-  tangent(4,2) += tau_row(4);
-  tangent(5,2) += tau_row(5);
-  tangent(4,3) += 0.5*tau_row(5);
-  tangent(5,3) += 0.5*tau_row(4);
-  tangent(5,4) += 0.5*tau_row(3);
+  (*tangent)(3,0) += tau_row(3);
+  (*tangent)(5,0) += tau_row(5);
+  (*tangent)(3,1) += tau_row(3);
+  (*tangent)(4,1) += tau_row(4);
+  (*tangent)(4,2) += tau_row(4);
+  (*tangent)(5,2) += tau_row(5);
+  (*tangent)(4,3) += 0.5*tau_row(5);
+  (*tangent)(5,3) += 0.5*tau_row(4);
+  (*tangent)(5,4) += 0.5*tau_row(3);
 
-  tangent(0,3) += tau_row(3);
-  tangent(0,5) += tau_row(5);
-  tangent(1,3) += tau_row(3);
-  tangent(1,4) += tau_row(4);
-  tangent(2,4) += tau_row(4);
-  tangent(2,5) += tau_row(5);
-  tangent(3,4) += 0.5*tau_row(5);
-  tangent(3,5) += 0.5*tau_row(4);
-  tangent(4,5) += 0.5*tau_row(3);
+  (*tangent)(0,3) += tau_row(3);
+  (*tangent)(0,5) += tau_row(5);
+  (*tangent)(1,3) += tau_row(3);
+  (*tangent)(1,4) += tau_row(4);
+  (*tangent)(2,4) += tau_row(4);
+  (*tangent)(2,5) += tau_row(5);
+  (*tangent)(3,4) += 0.5*tau_row(5);
+  (*tangent)(3,5) += 0.5*tau_row(4);
+  (*tangent)(4,5) += 0.5*tau_row(3);
   // cout << tangent <<endl;
 //
 };
@@ -175,17 +175,33 @@ void return_internalvar(double* STATEV,Matrix3d* ivar, int number_branches){
   for(int i=0;i<number_branches;++i){
     temp = *(ivar+i)-MatrixXd::Identity(3,3);
 
-    *(STATEV+9*i) = temp(0,0);
-    *(STATEV+9*i+1) = temp(0,1);
-    *(STATEV+9*i+2) = temp(0,2);
+    *(STATEV+10*i) = temp(0,0);
+    *(STATEV+10*i+1) = temp(0,1);
+    *(STATEV+10*i+2) = temp(0,2);
 
-    *(STATEV+9*i+3) = temp(1,0);
-    *(STATEV+9*i+4) = temp(1,1);
-    *(STATEV+9*i+5) = temp(1,2);
+    *(STATEV+10*i+3) = temp(1,0);
+    *(STATEV+10*i+4) = temp(1,1);
+    *(STATEV+10*i+5) = temp(1,2);
 
-    *(STATEV+9*i+6) = temp(2,0);
-    *(STATEV+9*i+7) = temp(2,1);
-    *(STATEV+9*i+8) = temp(2,2);
+    *(STATEV+10*i+6) = temp(2,0);
+    *(STATEV+10*i+7) = temp(2,1);
+    *(STATEV+10*i+8) = temp(2,2);
 
+  }
+};
+
+
+void add_all(Matrix3d* tau_final,Matrix6d* tangent_final,Matrix6d* tangent_volu_final,double* SSE,double* SCD,Matrix3d* tau,
+          Matrix6d* tangent,Matrix6d* tangent_volu,double* _SSE,double* _SCD ,int branches,int left){
+  *SSE = 0.0;
+  *SCD = 0.0;
+
+  for(int i=left;i< branches; ++i)
+  {
+    *tau_final = *tau_final + *(tau+i);
+    *tangent_final = *tangent_final + *(tangent+i);
+    *tangent_volu_final = *tangent_volu_final + *(tangent_volu+i);
+    *SSE += *(_SSE+i);
+    *SCD += *(_SCD+i);
   }
 };
--- USUBLIB.cpp
+++ USUBLIB.cpp
@@ -6,9 +6,13 @@
 #include <Eigen/Dense> // Eigen class
 #include <list>
 #include <string>
+#include <chrono>
 
 using namespace Eigen;
 using namespace std;
+using namespace std::chrono;
+
+typedef Matrix<double, 6, 6> Matrix6d;
 
 #include "Parser.h"
 #include "/home/vasudevan/PhD/Code/UMAT/perfect_incomp/Material.h"
@@ -23,89 +27,89 @@ int& NSTATV,double* PROPS,int& NPROPS,double* COORDS,double* DROT,double* PNEWDT
 double* DFGRD0,double* DFGRD1,int& NOEL,int& NPT,int& LAYER,int& KSPT,int& JSTEP,int& KINC)
 {
   // std::cout << NOEL << '\n';
-
+  // std::cout << DDSDDE << '\n';
   // std::cout << "Begin" << '\n';
 // PLANE STRESS ONLY
-  int number_branches = 10;
+  int number_branches = 0;
+  int left=0;
   MatrixXd F =  MatrixXd::Zero(3,3);
 	MatrixXd F_old =  MatrixXd::Zero(3,3);
   parse_deformation_variables(DFGRD0,DFGRD1,F,F_old);
-
   MatrixXd C = F.transpose()*F;
   MatrixXd b = F*F.transpose();
   MatrixXd C_inv = C.inverse();
+  // MatrixXd C_old = F_old.transpose()*F_old;
 
-  // std::cout << "C: " << '\n';
-  // std::cout << C << '\n';
   double stiff_ratio[number_branches];
   double relaxation_time[number_branches];
   parse_material_properties(PROPS,stiff_ratio,relaxation_time,number_branches);
 
-
-  // Matrix3d *ivar = Matrix3d[10];
   Matrix3d ivar[number_branches];
-
   parse_internal_variables(STATEV,ivar,number_branches);
 
+  viscous_branch branch[number_branches+1];
 
-	MatrixXd C_old = F_old.transpose()*F_old;
-
-
-	// viscous_branch* branch = viscous_branch[10];
-  viscous_branch branch[number_branches];
-
-  viscous_branch hyper_branch;
-  MatrixXd tau = MatrixXd::Zero(3,3);
-  Matrix3d temp_tau;
-  MatrixXd tangent = MatrixXd::Zero(6,6);
-  MatrixXd tangent_volu = MatrixXd::Zero(6,6);
+  Matrix3d tau[number_branches+1];
+  Matrix6d tangent[number_branches+1];
+  Matrix6d tangent_volu[number_branches+1];
+  double _SSE[number_branches+1];
+  double _SCD[number_branches+1];
 
 
+  Matrix3d tau_final = MatrixXd::Zero(3,3);
+  Matrix6d tangent_final = MatrixXd::Zero(6,6);
+  Matrix6d tangent_volu_final = MatrixXd::Zero(6,6);
+  // auto start = high_resolution_clock::now();
 
-  *SSE = 0.0;
-  *SCD = 0.0;
-  // //
-  // for(int i=0;i<number_branches;++i)
-  // {
-  //     // std::cout << i << '\n';
-  //     (branch+i)->delta_t = *DTIME;
-  //     (branch+i)->Material::set_material(stiff_ratio[i],relaxation_time[i]);
-  //     // std::cout << *(ivar+i) << '\n';
-  //     (branch+i)->set_inelastic_strain(*(ivar+i));
-  //     (branch+i)->compute_be_tr(F);
-  //     *(ivar+i)=(branch+i)->update_intervar_newton_principal(F);
-  //     temp_tau = (branch+i)->compute_stress_tau(branch[i].be);
-  //     tau += temp_tau;
-  //     tangent += (branch+i)->rotate_mat_tan(0);
-  //     tangent_volu += (branch+i)->mat_tan_volu();
-  //     *SSE += (branch+i)->compute_strain_energy(branch[i].be);
-  //     *SCD += (branch+i)->compute_dissipation();
-  //     // std::cout << *(ivar+i) << '\n';
   //
+  // #pragma omp parallel num_threads(number_branches+1)
+  // {
+  // #pragma omp for
+  for(int i=left;i<number_branches+1;++i)
+  {
+      if(i==number_branches)
+      {
+        (branch+i)->delta_t = *DTIME;
+        (branch+i)->Material::set_material(1.0,1E7);
+        (branch+i)->set_inelastic_strain(MatrixXd::Identity(3,3));
+        (branch+i)->compute_be_tr(F);
+        (branch+i)->update_intervar_newton_principal(F);
+        *(tau+i) = (branch+i)->compute_stress_tau();
+        *(tangent+i) = (branch+i)->rotate_mat_tan(1);
+        *(tangent_volu+i) = (branch+i)->mat_tan_volu();
+        *(_SSE+i) = (branch+i)->compute_strain_energy(branch[i].be);
+        *(_SCD+i) = (branch+i)->compute_dissipation();
+      }
+      else
+      {
+        (branch+i)->delta_t = *DTIME;
+        (branch+i)->Material::set_material(stiff_ratio[i],relaxation_time[i]);
+        (branch+i)->set_inelastic_strain(*(ivar+i));
+        (branch+i)->compute_be_tr(F);
+        // *(ivar+i)=(branch+i)->update_intervar_newton_principal_hencky(F);
+        (branch+i)->update_intervar_newton_principal(F);
+        *(tau+i) = (branch+i)->compute_stress_tau();
+        *(tangent+i) = (branch+i)->rotate_mat_tan(0);
+        *(tangent_volu+i) = (branch+i)->mat_tan_volu();
+        *(_SSE+i) = (branch+i)->compute_strain_energy_hencky(branch[i].be);
+        *(STATEV+10*i+9) = *(_SCD+i) = (branch+i)->compute_dissipation();
+      }
+  }
   // }
-  // std::cout << "Elastic Branch" << '\n';
-  hyper_branch.Material::set_material(1.0,1E7);
-  hyper_branch.set_inelastic_strain(MatrixXd::Identity(3,3));
-  hyper_branch.compute_be_tr(F);
-  hyper_branch.update_intervar_newton_principal(F);
-  tau += hyper_branch.compute_stress_tau(b);
-  tangent += hyper_branch.rotate_mat_tan(1);
-  tangent_volu += hyper_branch.mat_tan_volu();
-  // tangent_volu = 0.5*(tangent_volu+tangent_volu.transpose());
-  *SSE += hyper_branch.compute_strain_energy(b);
+  // auto stop = high_resolution_clock::now();
+  // auto duration = duration_cast<microseconds>(stop - start);
+  // cout << "duration = " << duration.count() << endl;
 
-  MatrixXd tau_temp = tau;
+  add_all(&tau_final,&tangent_final,&tangent_volu_final,SSE,SCD,tau,tangent,tangent_volu,_SSE,_SCD,number_branches+1,left);
 
-  deviatoric_projector(tau);
-  double p = -1.0*tau(2,2);
-  tau += p*MatrixXd::Identity(3,3);
-  deviatoric_projector_tangent(p,tau_temp,tangent,C_inv);
-  tangent = tangent + tangent_volu;
-  // std::cout << "tau " << '\n';
-  // std::cout << tau << '\n';
-  // throw;
-  return_stress(STRESS,tau);
-  return_tangent(DDSDDE,tangent);
+  MatrixXd tau_temp = tau_final;
+  deviatoric_projector(&tau_final);
+  double p = -1.0*tau_final(2,2);
+  tau_final += p*MatrixXd::Identity(3,3);
+  deviatoric_projector_tangent(p,tau_temp,&tangent_final);
+  tangent_final = tangent_final + tangent_volu_final;
+  return_stress(STRESS,tau_final);
+  return_tangent(DDSDDE,tangent_final);
   return_internalvar(STATEV,ivar,number_branches);
 
 return;
